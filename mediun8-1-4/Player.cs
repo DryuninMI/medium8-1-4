21bf60521b77aba5ad22a152c942eb5dd8c3aa2f
Михаил <messiahgamedesign@gmail.com>
2020-05-23 16:27:20 +0300

    Добавьте файлы проекта.

---
+++ mediun8-1-4/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mediun8_1_4
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public float WeaponCooldown { get; private set; }
+        public float MovementDirectionX { get; private set; }
+        public float MovementDirectionY { get; private set; }
+        public int Age { get; private set; }
+        public int WeaponDamage { get; private set; }
+        public float MovementSpeed { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
